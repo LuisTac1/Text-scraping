d678a83be7bf1e51286a41f87d9c3bfb0129d41e
Luis <luisedward157359@gmail.com>
2024-01-24 20:24:02 -0300

    Second commit

--- webBS.py
+++ webBS.py
@@ -8,6 +8,7 @@ requisition = requests.get(link, headers=hearders) # requisição do site e do n
 print(requisition)
 site = BeautifulSoup(requisition.text, "html.parser")
 
+# Description Scraping
 def first():
     print()
     page_title = site.find("title") #busca titulo h1
@@ -20,9 +21,10 @@ def first():
         print(i.get_text()) # Funcionou pegou todas as tags p da div desejada
     return first
 
+# Help Scraping
 def tag_help():
     print()
-    tag_div = site.find("div", id="getting-help") # busca div de getting-help
+    tag_div = site.find("div", id="getting-help") # find div from getting-help
     tag_h2 = tag_div.find(["h2"])
     print(tag_h2.get_text()[0:12])
     tag_p = tag_div.find_all(["p"])
@@ -31,14 +33,66 @@ def tag_help():
 
     return tag_help
 
-
-# to fix
+# Quick Start Scraping
 def quick_start():
     print()
-    tag_div = site.find(id="quick-start") # busca div de quick-start
-    tag_h2 = tag_div.find(["h2"])
-    print(tag_h2.get_text())
+    tag_div = site.find(id="quick-start") # find div from quick-start
+    tag_h2 = tag_div.find(["h1"])
+    print(tag_h2.get_text()[0:11])
 
+    tag_p = tag_div.find_all(["p"])[0:1]
+    for i in tag_p:
+        print(i.get_text())
+    print()
+    tag_div2 = tag_div.find_all("div", class_="highlight")[0:1] # Find div by div from one tag
+    for tag in tag_div2:
+        print(tag.get_text())
+
+
+    tag_p2 = tag_div.find_all(["p"])[1:2]
+    for i in tag_p2:
+        print()
+        print(i.get_text())
+    print()
+    tag_div3 = tag_div.find_all("div", class_="highlight")[1:2]
+    for tag in tag_div3:
+        print(tag.get_text())
+
+
+    tag_p3 = tag_div.find_all(["p"])[2:3]
+    for i in tag_p3:
+        print()
+        print(i.get_text())
+    print()
+    tag_div4 = tag_div.find_all("div", class_="highlight")[2:3]
+    for tag in tag_div4:
+        print(tag.get_text())
+
+
+    tag_p4 = tag_div.find_all(["p"])[3:4]
+    for i in tag_p4:
+        print()
+        print(i.get_text())
+    print()
+    tag_div2 = tag_div.find_all("div", class_="highlight")[3:4]
+    for tag in tag_div2:
+        print(tag.get_text())
+
+
+    tag_p5 = tag_div.find_all(["p"])[4:5]
+    for i in tag_p5:
+        print()
+        print(i.get_text())
+    print()
+    tag_div2 = tag_div.find_all("div", class_="highlight")[4:5]
+    for tag in tag_div2:
+        print(tag.get_text())
+
+
+    tag_p6 = tag_div.find_all(["p"])[5:6]
+    for i in tag_p6:
+        print()
+        print(i.get_text())
 
     return quick_start
 
@@ -46,7 +100,8 @@ def quick_start():
 
 first()
 tag_help()
-quick_start
+quick_start()
+
 
 
 # for tag in text_1:
